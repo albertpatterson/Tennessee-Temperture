49a0588ac2dd4982d0b145faba7678591997a932
albert patterson <apatterson189@gmail.com>
2022-07-20 16:07:40 -0500

    initial

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from fetch_data.fetch_and_save_all_data import fetch_and_save_all
-
+# from fetch_data.fetch_and_save_all_data import fetch_and_save_all
 # fetch_and_save_all()
+
+import parse_data.parse_all_history_data
